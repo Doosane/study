b93378b8225c4fe9ecb00c083be30a74513a5848
baekdoosan <doosan0425@gmail.com>
2024-11-27 22:35:14 +0900

    백준 문제 : BOJ4949번

--- src/main/java/com/doosane/study01/week02/day01/four/BOJ4949_Pair.java
+++ src/main/java/com/doosane/study01/week02/day01/four/BOJ4949_Pair.java
@@ -2,6 +2,9 @@ package com.doosane.study01.week02.day01.four;
 
 import java.io.IOException;
 
+/**
+ * 균형 잡힌 세상
+ */
 public class BOJ4949_Pair {
     public static void main(String[] args) throws IOException {
 
